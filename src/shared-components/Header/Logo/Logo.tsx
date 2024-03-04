4eab58aa797bec8cf8ffdac8382658f9ba879105
Ekalape <ekalape@outlook.it>
2024-03-04 16:13:46 +0100

    feat: add loader, adjust styles, increase video loaded

--- src/shared-components/Header/Logo/Logo.tsx
+++ src/shared-components/Header/Logo/Logo.tsx
@@ -2,7 +2,7 @@ import './styles.scss';
 
 export function Logo() {
   return (
-    <div className='logo-container container flex items-center justify-center  text-white hover:text-red-600 '>
+    <div className='logo-container container flex items-center justify-center  hover:text-white text-red-600 '>
       <i className='ring'></i>
       <i className='ring'></i>
       <i className='ring'></i>
